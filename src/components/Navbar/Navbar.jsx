793bb341831c27317c6488fbbffb6976f5bb9d6e
Garcia Edgar W <wili_2808@hotmail.com>
2024-12-01 17:19:33 -0300

    side bar, continue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,47 @@
 import './navbar.css'
+;<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu"
+/>
 export function Navbar() {
   return (
     <div className="nav-container">
-      <nav className="navbar">
+      <div className="nav-btn">
+        <svg
+          className="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="35px"
+          viewBox="0 -960 960 960"
+          width="40px"
+          fill="#e8eaed"
+        >
+          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+        </svg>
+        <span className="nav-titulo">ORION BAR</span>
+      </div>
+
+      <div className="navbar">
         <ul>
-          <li>HOME</li>
-          <li>NOSOTROS</li>
-          <li>NUESTRA CARTA</li>
-          <li>EVENTOS</li>
-          <li>SHOP</li>
-          <li>CONTACTO</li>
+          <li>
+            <span>HOME</span>
+          </li>
+          <li>
+            <span>NOSOTROS</span>
+          </li>
+          <li>
+            <span>NUESTRA CARTA</span>
+          </li>
+          <li>
+            <span>EVENTOS</span>
+          </li>
+          <li>
+            <span>SHOP</span>
+          </li>
+          <li>
+            <span>CONTACTO</span>
+          </li>
         </ul>
-      </nav>
+      </div>
     </div>
   )
 }
