1764d6eb5ebad233e6e0a0b382dc6c54757a20f2
Garcia Edgar W. <wili_2808@hotmail.com>
2025-01-04 22:36:23 -0300

    ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
+import { useState } from 'react'
 import './navbar.css'
-;<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu"
-/>
+const LINK_INSTA = 'https://www.instagram.com/orion.barber.bar/'
+const ADRESS_BAR =
+  'https://www.google.com/maps/place/Francisco+L%C3%B3pez+1855,+Ituzaing%C3%B3,+Corrientes/@-27.5853111,-56.6909163,18z/data=!3m1!4b1!4m5!3m4!1s0x9457388c4f307bc9:0xc521b90afef5589e!8m2!3d-27.5853111!4d-56.6896288?hl=es&entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D'
 export function Navbar() {
+  const [expandMenu, setExpandMenu] = useState(false)
+  const handleClick = () => {
+    setExpandMenu(!expandMenu)
+  }
   return (
     <div className="nav-container">
       <div className="nav-btn">
         <svg
           className="icon"
+          onClick={handleClick}
           xmlns="http://www.w3.org/2000/svg"
-          height="35px"
           viewBox="0 -960 960 960"
-          width="40px"
-          fill="#e8eaed"
         >
-          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+          <path d="M480-200 240-440l56-56 184 183 184-183 56 56-240 240Zm0-240L240-680l56-56 184 183 184-183 56 56-240 240Z" />
         </svg>
-        <span className="nav-titulo">ORION BAR</span>
       </div>
 
-      <div className="navbar">
+      <div className={expandMenu ? 'navbar-hidden' : 'navbar'}>
         <ul>
           <li>
             <span>HOME</span>
